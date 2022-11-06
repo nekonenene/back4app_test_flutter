30459af3122bf3bb495219603dda240e5acd5158
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-11-06 23:05:00 +0900

    Add logger and error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger();
 
 void main() async {
   // To load the .env file contents into dotenv.
   // NOTE: fileName defaults to .env and can be omitted in this case.
   // Ensure that the filename corresponds to the path in step 1 and 2.
-  await dotenv.load(fileName: '.env');
+  try {
+    await dotenv.load(fileName: '.env');
+    logger.d('BACK4APP_APP_ID: ${dotenv.env['BACK4APP_APP_ID']}');
+  } catch (e) {
+    logger.e(e);
+  }
 
   runApp(const MyApp());
 }
